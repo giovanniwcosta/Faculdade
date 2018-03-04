e8b52312605dbfeeb5fc6c323d4b1eff67c77e63
Giovannidpc <giovanniwcosta@usp.br>
2018-03-04 19:04:23 -0300

    Exer1

---
+++ Projetos/Projetos1/exer1.f90
@@ -0,0 +1,13 @@
+program presicao
+    implicit none
+    real*8 ad
+    real as
+    integer j
+    ad = 2.0d0
+    as = 2.0
+    do j=1,52
+        ad = ad*2.0d0
+        as = as*2.0
+        write(*,*)j,(1.0d0 + (2.0d0/(ad))),(1.0 + (2.0/(as)))
+    enddo
+end program
