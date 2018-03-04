9c303ff2ac73c45b441522aef74716068b587e00
Giovannidpc <giovanniwcosta@usp.br>
2018-03-04 20:39:25 -0300

    Versão final do primeiro exercício de IntroFisComp.

--- Projetos/Projetos1/exer1.f90
+++ Projetos/Projetos1/exer1.f90
@@ -1,10 +1,3 @@
-!Vamos determinar aprecisão do seu computador usando aritmética de ponto
-!flutuante em precisão simples e dupla.  Para isso, crie um “loop” que soma
-!uma variável ao número 1 e testa se, à medida que a diminui, o resultado da
-!soma difere do número 1.  O menor valor que pode ser detectado para essa
-!varaavel é a chamada precisão  de máquina. Inicie seu programa com a = 1 e
-!vá dividindo o valor por 2 a cada iteração. Faça isso para variável de tipo
-!real com precião simples e dupla.
 program presicao
     implicit none
     real*8 ad,vd,r,a,pi
