a29723b3ee1222c617c2217b9b46165814b97f1f
Giovannidpc <giovanniwcosta@usp.br>
2018-03-04 19:21:35 -0300

    Exer1-3

--- Projetos/Projetos1/exer1.f90
+++ Projetos/Projetos1/exer1.f90
@@ -1,13 +1,23 @@
+!Vamos determinar aprecisão do seu computador usando aritmética de ponto
+!flutuante em precisão simples e dupla.  Para isso, crie um “loop” que soma
+!uma variável ao número 1 e testa se, à medida que a diminui, o resultado da
+!soma difere do número 1.  O menor valor que pode ser detectado para essa
+!varaavel é a chamada precisão  de máquina. Inicie seu programa com a = 1 e
+!vá dividindo o valor por 2 a cada iteração.
 program presicao
     implicit none
-    real*8 ad
-    real as
-    integer 2
+    real*8 ad,vd
+    real as,vs
+    integer j
     ad = 2.0d0
     as = 2.0
-    do j=1,52
+    do j=1,60
         ad = ad*2.0d0
         as = as*2.0
-        write(*,*)j,(1.0d0 + (2.0d0/(ad))),(1.0 + (2.0/(as)))
+        vd = 1.0d0 + 2.0d0/(ad)
+        vs = 1.0 + 2.0/(as)
+        if(vs == 1.0)vs = 0
+        if(vd == 1.0d0) stop
+        write(*,*)j,vd,vs
     enddo
 end program
