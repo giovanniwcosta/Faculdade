12536c66e93edf2d3159a12ebd7eab5167a8b777
Giovannidpc <giovanniwcosta@usp.br>
2018-03-06 10:18:05 -0300

    Exer1-Final

--- Projetos/Projetos1/exer1.f90
+++ Projetos/Projetos1/exer1.f90
@@ -5,26 +5,27 @@ program exer1
     real as,vs
 
     read(*,*)r
-    ad = 1.0d0
-    as = 1.0
-
+    as = 1.0/2.0
+    ad = 1.0d0/2.0d0
+    vs = 1.0 + as
+    vd = 1.0d0 + ad
     write(*,*)"Precisão Simples"
-    do while(1>0)
+    do while(vs > 1.0)
+        write(*,*)as,vs
         as = as/2.0
         vs = 1.0 + as
-        write(*,*)as,vs
-        if(vs == 1.0)goto 1
     enddo
+    write(*,*)as,vs
 
-    1 write(*,*)"Precisão Dupla"
-    do while(1>0)
+    write(*,*)"Precisão Dupla"
+    do while(vd > 1.0d0)
+        write(*,*)ad,vd
         ad = ad/2.0d0
         vd = 1.0d0 + ad
-        write(*,*)ad,vd
-        if(vd == 1.0d0)goto 2
     enddo
+    write(*,*)ad,vd
 
-    2 pi = 4*atan(1.0_8)
+    pi = 4*atan(1.0_8)
     a = pi*r**2.0d0
     write(*,*)"Valor da Area:",a
 
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Faculdade
-Projetos e exercícios desenvolvidos ao longo do primeiro semestre de 2018
+Aqui estão os rojetos e exercícios desenvolvidos ao longo do primeiro semestre de 2018, no curso de Física Computacional:USP São Carlos.
+No branch IntroFisComp estão os 6 projetos que servem como avalição para o semetre do curso de Introdução a física computacional, que tem como enfoque
+capacitar os alunos a resolver problemas de física de forma numérica para alcançar respostar com maior precisão e resolver problemas que não apresentam solução analítica.
